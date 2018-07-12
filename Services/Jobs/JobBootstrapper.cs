572657858fe6b1b72359533e982aa9c3f2f86344
Fergal Moran <fergal.moran@gmail.com>
2018-07-12 14:13:27 +0100

    Disable clear orphan job (for now)

--- Services/Jobs/JobBootstrapper.cs
+++ Services/Jobs/JobBootstrapper.cs
@@ -8,7 +8,7 @@ using PodNoms.Api.Persistence;
 namespace PodNoms.Api.Services.Jobs {
     public static class JobBootstrapper {
         public static void BootstrapJobs() {
-            RecurringJob.AddOrUpdate<ClearOrphanAudioJob>(x => x.Execute(), Cron.Daily(1));
+            // RecurringJob.AddOrUpdate<ClearOrphanAudioJob>(x => x.Execute(), Cron.Daily(1));
             RecurringJob.AddOrUpdate<UpdateYouTubeDlJob>(x => x.Execute(), Cron.Daily(1, 30));
             RecurringJob.AddOrUpdate<ProcessPlaylistsJob>(x => x.Execute(), Cron.Daily(2));
 
