31f64746b66b32858b56381679e4a011b245400a
Fergal Moran <fergal.moran@gmail.com>
2018-10-08 22:36:27 +0100

    Added ze robots

---
+++ podnoms-api/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PodNoms.Api.Controllers {
+    public class HomeController : Controller {
+        [Route("robots.txt", Name = "GetRobotsText")]
+        [Route("rss/robots.txt", Name = "GetRobotsTextRss")]
+        public ContentResult RobotsText() {
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine("user-agent: *");
+            stringBuilder.AppendLine("disallow: /error/");
+            stringBuilder.AppendLine("allow: /error/foo");
+
+            return this.Content(stringBuilder.ToString(), "text/plain", Encoding.UTF8);
+        }
+    }
+}
