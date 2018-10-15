bacaa338cda66d2f4ada67121e79397d8f81e8a1
Fergal Moran <fergal.moran@gmail.com>
2018-10-15 11:02:23 +0100

    Pages stuff

---
+++ PodNoms.Pages/PodNoms.Pages.Server/Controllers/PagesController.cs
@@ -0,0 +1,15 @@
+﻿using PodNoms.Pages.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PodNoms.Pages.Server.Controllers {
+    [Route("api/[controller]")]
+    public class PagesController : Controller {
+        [HttpGet("[action]")]
+        public IEnumerable<WeatherForecast> Pages() {
+        }
+    }
+}
---
+++ podnoms-api/Controllers/Public/PublicPodcastController.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PodNoms.Common.Data.ViewModels.Resources;
+using PodNoms.Common.Persistence.Repositories;
+using PodNoms.Data.Models;
+
+namespace PodNoms.Api.Controllers.Public {
+    [Route("pub/podcast")]
+    public class PublicPodcastController : Controller {
+        private readonly IPodcastRepository _podcastRepository;
+        private readonly IMapper _mapper;
+
+        public PublicPodcastController(IPodcastRepository podcastRepository, IMapper mapper) {
+            _podcastRepository = podcastRepository;
+            _mapper = mapper;
+        }
+        [HttpGet("{user}/{podcast}")]
+        public async Task<ActionResult<List<PodcastViewModel>>> Get(string user, string podcast) {
+            var results = await _podcastRepository.GetAll()
+                .Where(r => r.AppUser.Slug == user && r.Slug == podcast)
+                .ToListAsync();
+            return _mapper.Map<List<Podcast>, List<PodcastViewModel>>(results);
+        }
+    }
+}
