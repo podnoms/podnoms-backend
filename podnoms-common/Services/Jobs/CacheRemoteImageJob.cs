3c264e5ac823cfaceb7a7800d0cd5004c76e86d2
Fergal Moran <fergal.moran@gmail.com>
2019-05-08 21:12:04 +0100

    Update azure storage libs

--- podnoms-common/Services/Jobs/CacheRemoteImageJob.cs
+++ podnoms-common/Services/Jobs/CacheRemoteImageJob.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Microsoft.WindowsAzure.Storage;
 using PodNoms.Common.Data.Settings;
 using PodNoms.Common.Persistence;
 using PodNoms.Common.Persistence.Repositories;
@@ -20,7 +19,7 @@ namespace PodNoms.Common.Services.Jobs {
         private readonly ImageFileStorageSettings _imageFileStoragesSettings;
         private readonly ILogger _logger;
 
-        public CacheRemoteImageJob(IEntryRepository entryRepository,
+        public CacheRemoteImageJob (IEntryRepository entryRepository,
             IFileUploader fileUploader,
             IOptions<StorageSettings> storageSettings,
             IOptions<ImageFileStorageSettings> imageFileStoragesSettings,
@@ -31,72 +30,69 @@ namespace PodNoms.Common.Services.Jobs {
             _unitOfWork = unitOfWork;
             _storageSettings = storageSettings.Value;
             _imageFileStoragesSettings = imageFileStoragesSettings.Value;
-            _logger = logger.CreateLogger<CacheRemoteImageJob>();
+            _logger = logger.CreateLogger<CacheRemoteImageJob> ();
         }
 
-        public async Task<bool> Execute() {
+        public async Task<bool> Execute () {
             var images = _entryRepository
-                .GetAll();
-//                .Where(r => r.Id == Guid.Parse("1ba9eeae-77ab-48d2-abb1-c758c4780d5e"));
+                .GetAll ();
 
             int i = 1;
-            int count = images.Count();
+            int count = images.Count ();
             foreach (var e in images) {
-                _logger.LogDebug($"Caching image for: {e.Id}");
-                var exists = await _fileUploader.FileExists(_imageFileStoragesSettings.ContainerName,
+                _logger.LogDebug ($"Caching image for: {e.Id}");
+                var exists = await _fileUploader.FileExists (_imageFileStoragesSettings.ContainerName,
                     $"entry/cached/{e.Id.ToString()}-32x32.png");
                 if (!exists) {
-                    _logger.LogDebug($"Caching: {e.Id}");
-                    var imageUrl = e.ImageUrl.StartsWith("http")
-                        ? e.ImageUrl
-                        : $"{_storageSettings.CdnUrl}/{e.ImageUrl}";
-                    await CacheImage(
+                    _logger.LogDebug ($"Caching: {e.Id}");
+                    var imageUrl = e.ImageUrl.StartsWith ("http") ?
+                        e.ImageUrl :
+                        $"{_storageSettings.CdnUrl}/{e.ImageUrl}";
+                    await CacheImage (
                         imageUrl,
-                        e.Id.ToString());
+                        e.Id.ToString ());
+                } else {
+                    _logger.LogDebug ($"Not caching: {e.Id}");
                 }
-                else {
-                    _logger.LogDebug($"Not caching: {e.Id}");
-                }
-                _logger.LogDebug($"Processing {i++} of {count}");
+                _logger.LogDebug ($"Processing {i++} of {count}");
             }
 
             return true;
         }
 
-        public async Task<(string, string)> CacheImage(Guid entryId) {
-            var entry = await _entryRepository.GetAsync(entryId);
+        public async Task < (string, string) > CacheImage (Guid entryId) {
+            var entry = await _entryRepository.GetAsync (entryId);
             if (entry is null) return (string.Empty, string.Empty);
 
-            var (original, thumbnail) = await CacheImage(entry.ImageUrl, entry.Id.ToString());
+            var (original, thumbnail) = await CacheImage (entry.ImageUrl, entry.Id.ToString ());
 
-            if (string.IsNullOrEmpty(original)) return (string.Empty, string.Empty);
+            if (string.IsNullOrEmpty (original)) return (string.Empty, string.Empty);
 
             entry.ImageUrl = original;
-            await _unitOfWork.CompleteAsync();
+            await _unitOfWork.CompleteAsync ();
 
             return (original, thumbnail);
         }
 
-        public async Task<(string, string)> CacheImage(string imageUrl, string destUid) {
+        public async Task < (string, string) > CacheImage (string imageUrl, string destUid) {
             try {
-                var sourceFile = await HttpUtils.DownloadFile(imageUrl);
-                var thumbnailFile = ImageUtils.CreateThumbnail(sourceFile, destUid, 32, 32);
+                var sourceFile = await HttpUtils.DownloadFile (imageUrl);
+                var thumbnailFile = ImageUtils.CreateThumbnail (sourceFile, destUid, 32, 32);
 
-                var original = await _fileUploader.UploadFile(
+                var original = await _fileUploader.UploadFile (
                     sourceFile,
                     _imageFileStoragesSettings.ContainerName,
                     $"entry/{destUid}.png",
                     "image/png", null);
-                var thumbnail = await _fileUploader.UploadFile(
+                var thumbnail = await _fileUploader.UploadFile (
                     thumbnailFile,
                     _imageFileStoragesSettings.ContainerName,
                     $"entry/cached/{destUid}-32x32.png",
                     "image/png", null);
 
                 return (original, thumbnail);
-            }
-            catch (Exception ex) {
-                _logger.LogError($"Error caching image: {ex.Message}");
+            } catch (Exception ex) {
+                _logger.LogError ($"Error caching image: {ex.Message}");
             }
 
             return (string.Empty, string.Empty);
