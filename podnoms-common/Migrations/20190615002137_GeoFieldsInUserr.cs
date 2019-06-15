fe5da6032de2d84d4ce1dfe30a6f7b9bc088a9d9
Fergal Moran <fergal.moran@gmail.com>
2019-06-15 01:48:33 +0100

    Finish up geocoding

---
+++ podnoms-common/Migrations/20190615002137_GeoFieldsInUserr.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PodNoms.Comon.Migrations
+{
+    public partial class GeoFieldsInUserr : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Country",
+                table: "AspNetUsers",
+                newName: "Zip");
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CountryCode",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CountryName",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RegionCode",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RegionName",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CountryCode",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CountryName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "RegionCode",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "RegionName",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "Zip",
+                table: "AspNetUsers",
+                newName: "Country");
+        }
+    }
+}
--- podnoms-data/Models/ApplicationUser.cs
+++ podnoms-data/Models/ApplicationUser.cs
@@ -24,6 +24,14 @@ namespace PodNoms.Data.Models {
 
         public DateTime LastSeen { get; set; }
         public string IpAddress { get; set; }
-        public string Country { get; set; }
+        public string CountryCode { get; set; }
+        public string CountryName { get; set; }
+        public string RegionCode { get; set; }
+        public string RegionName { get; set; }
+        public string City { get; set; }
+        public string Zip { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+
     }
 }
