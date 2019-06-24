39e05245a7ccc67d5a59c6366c0b7b7b7f231da0
Fergal Moran <fergal.moran@gmail.com>
2019-06-25 00:57:40 +0100

    Job corrected

--- podnoms-common/Services/Jobs/CacheRemoteImageJob.cs
+++ podnoms-common/Services/Jobs/CacheRemoteImageJob.cs
@@ -9,6 +9,8 @@ using PodNoms.Common.Persistence.Repositories;
 using PodNoms.Common.Services.Storage;
 using PodNoms.Common.Utils;
 using PodNoms.Data.Models;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 
 namespace PodNoms.Common.Services.Jobs {
     public class CacheRemoteImageJob : IJob {
@@ -35,67 +37,61 @@ namespace PodNoms.Common.Services.Jobs {
 
         public async Task<bool> Execute () {
             var images = _entryRepository
-                .GetAll ();
+                .GetAll ()
+                .Where (r => r.Processed == true)
+                .Where (r => r.ImageUrl.StartsWith ("https://i.ytimg.com/"));
 
             int i = 1;
             int count = images.Count ();
             foreach (var e in images) {
                 _logger.LogDebug ($"Caching image for: {e.Id}");
-                var exists = await _fileUploader.FileExists (_imageFileStoragesSettings.ContainerName,
-                    $"entry/cached/{e.Id.ToString()}-32x32.png");
-                if (!exists) {
-                    _logger.LogDebug ($"Caching: {e.Id}");
-                    var imageUrl = e.ImageUrl.StartsWith ("http") ?
-                        e.ImageUrl :
-                        $"{_storageSettings.CdnUrl}/{e.ImageUrl}";
-                    await CacheImage (
-                        imageUrl,
-                        e.Id.ToString ());
-                } else {
-                    _logger.LogDebug ($"Not caching: {e.Id}");
-                }
+                _logger.LogDebug ($"Caching: {e.Id}");
+                await CacheImage (e.Id);
                 _logger.LogDebug ($"Processing {i++} of {count}");
             }
 
             return true;
         }
 
-        public async Task < (string, string) > CacheImage (Guid entryId) {
+        public async Task<string> CacheImage (Guid entryId) {
             var entry = await _entryRepository.GetAsync (entryId);
-            if (entry is null) return (string.Empty, string.Empty);
+            if (entry is null) return string.Empty;
 
-            var (original, thumbnail) = await CacheImage (entry.ImageUrl, entry.Id.ToString ());
+            var file = await CacheImage (entry.ImageUrl, entry.Id.ToString ());
 
-            if (string.IsNullOrEmpty (original)) return (string.Empty, string.Empty);
+            if (string.IsNullOrEmpty (file)) return string.Empty;
 
-            entry.ImageUrl = original;
+            entry.ImageUrl = file;
             await _unitOfWork.CompleteAsync ();
 
-            return (original, thumbnail);
+            return file;
         }
 
-        public async Task < (string, string) > CacheImage (string imageUrl, string destUid) {
+        public async Task<string> CacheImage (string imageUrl, string destUid) {
+            // TODO: Need to convert everything to jpeg
+            // PNG was a bad choice
             try {
                 var sourceFile = await HttpUtils.DownloadFile (imageUrl);
-                var thumbnailFile = ImageUtils.CreateThumbnail (sourceFile, destUid, 32, 32);
+                if (string.IsNullOrEmpty (sourceFile))
+                    return string.Empty;
+                var extension = await HttpUtils.GetUrlExtension (imageUrl);
 
-                var original = await _fileUploader.UploadFile (
+                if (!extension.Equals ("jpg")) {
+                    (sourceFile, extension) = ImageUtils.ConvertFile (sourceFile, sourceFile, "jpg");
+                }
+
+                var remoteFile = await _fileUploader.UploadFile (
                     sourceFile,
                     _imageFileStoragesSettings.ContainerName,
-                    $"entry/{destUid}.png",
-                    "image/png", null);
-                var thumbnail = await _fileUploader.UploadFile (
-                    thumbnailFile,
-                    _imageFileStoragesSettings.ContainerName,
-                    $"entry/cached/{destUid}-32x32.png",
-                    "image/png", null);
+                    $"entry/{destUid}.jpg",
+                    "image/jpeg", null);
 
-                return (original, thumbnail);
+                return remoteFile;
             } catch (Exception ex) {
                 _logger.LogError ($"Error caching image: {ex.Message}");
             }
 
-            return (string.Empty, string.Empty);
+            return string.Empty;
         }
     }
 }
--- podnoms-common/Services/Jobs/JobBootstrapper.cs
+++ podnoms-common/Services/Jobs/JobBootstrapper.cs
@@ -4,12 +4,13 @@ using Hangfire;
 namespace PodNoms.Common.Services.Jobs {
     public static class JobBootstrapper {
         public static void BootstrapJobs (bool isDevelopment) {
-            if (!isDevelopment) {
+            if (!isDevelopment || true) {
                 RecurringJob.AddOrUpdate<GeocodeUsersJob> (x => x.Execute (null), Cron.Hourly);
                 RecurringJob.AddOrUpdate<DeleteOrphanAudioJob> (x => x.Execute (), Cron.Daily (1));
                 RecurringJob.AddOrUpdate<UpdateYouTubeDlJob> (x => x.Execute (), Cron.Daily (1, 30));
                 RecurringJob.AddOrUpdate<ProcessPlaylistsJob> (x => x.Execute (), Cron.Daily (2));
                 RecurringJob.AddOrUpdate<ProcessFailedPodcastsJob> (x => x.Execute (), Cron.Daily (2, 30));
+                RecurringJob.AddOrUpdate<CacheRemoteImageJob> (x => x.Execute (), Cron.Daily (3));
             }
         }
     }
