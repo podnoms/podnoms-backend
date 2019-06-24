e33e5877b9135c544a5ee6964757d67e0e8a9e21
Fergal Moran <fergal.moran@gmail.com>
2019-06-24 22:26:09 +0100

    Sync issues

--- podnoms-common/Services/Jobs/JobBootstrapper.cs
+++ podnoms-common/Services/Jobs/JobBootstrapper.cs
@@ -4,8 +4,8 @@ using Hangfire;
 namespace PodNoms.Common.Services.Jobs {
     public static class JobBootstrapper {
         public static void BootstrapJobs (bool isDevelopment) {
-            RecurringJob.AddOrUpdate<GeocodeUsersJob> (x => x.Execute (null), Cron.Hourly);
             if (!isDevelopment) {
+                RecurringJob.AddOrUpdate<GeocodeUsersJob> (x => x.Execute (null), Cron.Hourly);
                 RecurringJob.AddOrUpdate<DeleteOrphanAudioJob> (x => x.Execute (), Cron.Daily (1));
                 RecurringJob.AddOrUpdate<UpdateYouTubeDlJob> (x => x.Execute (), Cron.Daily (1, 30));
                 RecurringJob.AddOrUpdate<ProcessPlaylistsJob> (x => x.Execute (), Cron.Daily (2));
--- podnoms-common/Services/Middleware/UserLoggingFilter.cs
+++ podnoms-common/Services/Middleware/UserLoggingFilter.cs
@@ -12,34 +12,36 @@ namespace PodNoms.Common.Services.Middleware {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly ILogger _logger;
 
-        public UserLoggingFilter (UserManager<ApplicationUser> userManager, ILogger<UserLoggingFilter> logger) {
+        public UserLoggingFilter(UserManager<ApplicationUser> userManager, ILogger<UserLoggingFilter> logger) {
             _userManager = userManager;
             _logger = logger;
         }
 
-        public async Task OnActionExecutionAsync (ActionExecutingContext context, ActionExecutionDelegate next) {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next) {
             if (context.HttpContext.User.Identity.IsAuthenticated) {
 
                 string userId = null;
 
-                var claimsIdentity = (ClaimsIdentity) context.HttpContext.User.Identity;
-                var userIdClaim = claimsIdentity.Claims.SingleOrDefault (c => c.Type == ClaimTypes.NameIdentifier);
+                var claimsIdentity = (ClaimsIdentity)context.HttpContext.User.Identity;
+                var userIdClaim = claimsIdentity.Claims.SingleOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
                 if (userIdClaim != null) {
                     userId = userIdClaim.Value;
                 }
 
-                var user = await _userManager.FindByNameAsync (userId);
-                var header = context.HttpContext.Request.Headers["X-Forwarded-For"].SingleOrDefault ();
-                if (header != null) {
-                    //might be coming to us from a proxy
-                    user.IpAddress = header;
-                } else {
-                    user.IpAddress = context.HttpContext.Connection.RemoteIpAddress.ToString ();
+                var user = await _userManager.FindByNameAsync(userId);
+                if (user != null) {
+                    var header = context.HttpContext.Request.Headers["X-Forwarded-For"].SingleOrDefault();
+                    if (header != null) {
+                        //might be coming to us from a proxy
+                        user.IpAddress = header;
+                    } else {
+                        user.IpAddress = context.HttpContext.Connection.RemoteIpAddress.ToString();
+                    }
+                    user.LastSeen = System.DateTime.Now;
+                    await _userManager.UpdateAsync(user);
                 }
-                user.LastSeen = System.DateTime.Now;
-                await _userManager.UpdateAsync (user);
             }
-            var resultContext = await next ();
+            var resultContext = await next();
         }
     }
 
