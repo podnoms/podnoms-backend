25045c83c8552bb28886f798eb5efce0506ab406
Fergal Moran <fergal.moran@gmail.com>
2019-07-15 00:24:16 +0100

    Fixed some broken job processor stuff

--- podnoms-jobs/Program.cs
+++ podnoms-jobs/Program.cs
@@ -14,7 +14,17 @@ namespace PodNoms.Jobs {
             WebHost.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration((context, config) => {
                     if (context.HostingEnvironment.IsProduction()) {
+                        Console.WriteLine("Production instance bootstrapping");
+                        config.SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json", optional: false)
+                            .AddJsonFile($"appsettings.Production.json", optional: true)
+                            .AddEnvironmentVariables("ASPNETCORE_");
                         var builtConfig = config.Build();
+
+                        Console.WriteLine($"Vault: {builtConfig["KeyVaultSettings:Vault"]}");
+                        Console.WriteLine($"ClientId: {builtConfig["KeyVaultSettings:ClientId"]}");
+                        Console.WriteLine($"ClientSecret: {builtConfig["KeyVaultSettings:ClientSecret"]}");
+
                         config.AddAzureKeyVault(
                             $"https://{builtConfig["KeyVaultSettings:Vault"]}.vault.azure.net/",
                             builtConfig["KeyVaultSettings:ClientId"],
--- podnoms-jobs/Services/PodNomsApiNotificationService.cs
+++ podnoms-jobs/Services/PodNomsApiNotificationService.cs
@@ -25,6 +25,10 @@ namespace PodNoms.Jobs.Services {
 
         public async Task NotifyUser(string token, string userId, string title, string body, string target, string image) {
             try {
+                if (string.IsNullOrEmpty(token)) {
+                    _logger.LogWarning("Unable to NotifiyUser as no valid auth token");
+                    return;
+                }
                 var urlString = $"userId=userId&title={title}&body={body}&target={target}&image={image}";
                 _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
                 var result = await _httpClient.PostAsync(
@@ -44,13 +48,17 @@ namespace PodNoms.Jobs.Services {
         }
 
         public async Task SendCustomNotifications(string token, Guid podcastId, string title, string body, string url) {
-            var payload = JsonConvert.SerializeObject(new {
-                podcastId = podcastId.ToString(),
-                title = title,
-                url = url
-            });
-            _logger.LogDebug("Sending message", payload);
             try {
+                if (string.IsNullOrEmpty(token)) {
+                    _logger.LogWarning("Unable to SendCustomNotifications as no valid auth token");
+                    return;
+                }
+                var payload = JsonConvert.SerializeObject(new {
+                    podcastId = podcastId.ToString(),
+                    title = title,
+                    url = url
+                });
+                _logger.LogDebug("Sending message", payload);
                 var urlString = $"podcastId={podcastId}&title={title}&body={body}&url={url}";
                 _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
                 var result = await _httpClient.PostAsync(
