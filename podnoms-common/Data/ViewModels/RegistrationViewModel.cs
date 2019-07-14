063d739fd2f894ff627034b948f4c3672db3b3af
Fergal Moran <fergal.moran@gmail.com>
2019-07-14 19:08:57 +0100

    Username/Password slug flow working

--- podnoms-common/Data/ViewModels/RegistrationViewModel.cs
+++ podnoms-common/Data/ViewModels/RegistrationViewModel.cs
@@ -1,5 +1,7 @@
 namespace PodNoms.Common.Data.ViewModels {
     public class RegistrationViewModel {
+        //TODO: Rename Identifier, 
+        public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public string FirstName { get; set; }
