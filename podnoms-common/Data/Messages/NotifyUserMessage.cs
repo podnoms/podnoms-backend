29aca49c67a9820e8498a9cbb102f0102937c4fc
Fergal Moran <fergal.moran@gmail.com>
2019-07-24 22:48:50 +0100

    Queue working

---
+++ podnoms-common/Data/Messages/NotifyUserMessage.cs
@@ -0,0 +1,12 @@
+﻿using EasyNetQ;
+
+namespace PodNoms.Common.Data.Messages {
+    [Queue("PodNoms.Client", ExchangeName = "PodNoms.Client")]
+    public sealed class NotifyUserMessage {
+        public string UserId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Target { get; set; }
+        public string Image { get; set; }
+    }
+}
