4155452155f55183b42fdb6af134f1ffd7d9f9d4
Fergal Moran <fergal.moran@gmail.com>
2019-07-29 14:36:31 +0100

    Remove playlists job for now

--- podnoms-common/Services/Jobs/JobBootstrapper.cs
+++ podnoms-common/Services/Jobs/JobBootstrapper.cs
@@ -7,7 +7,7 @@ namespace PodNoms.Common.Services.Jobs {
             if (!isDevelopment) {
                 RecurringJob.AddOrUpdate<DeleteOrphanAudioJob>(x => x.Execute(), Cron.Daily(1));
                 RecurringJob.AddOrUpdate<UpdateYouTubeDlJob>(x => x.Execute(), Cron.Daily(1, 30));
-                RecurringJob.AddOrUpdate<ProcessPlaylistsJob>(x => x.Execute(), Cron.Daily(2));
+                //RecurringJob.AddOrUpdate<ProcessPlaylistsJob>(x => x.Execute(), Cron.Daily(2));
                 RecurringJob.AddOrUpdate<ProcessFailedPodcastsJob>(x => x.Execute(), Cron.Daily(2, 30));
                 RecurringJob.AddOrUpdate<ProcessFailedPlaylistEntryJob>(x => x.Execute(), Cron.Daily(3));
                 RecurringJob.AddOrUpdate<CacheRemoteImageJob>(x => x.Execute(), Cron.Daily(3, 10));
