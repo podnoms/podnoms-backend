e53f38f049587961d8555a592e94ebdcc8ce8c5d
Fergal Moran <fergal.moran@gmail.com>
2019-08-02 21:06:20 +0100

    Re-enable full playlist processing

--- podnoms-common/Services/Jobs/ProcessPlaylistsJob.cs
+++ podnoms-common/Services/Jobs/ProcessPlaylistsJob.cs
@@ -62,8 +62,8 @@ namespace PodNoms.Common.Services.Jobs {
         public async Task<bool> Execute(PerformContext context) {
             _logger.LogDebug("Starting playlist processing");
             context.WriteLine("Starting playlist processing");
-            var playlists = _playlistRepository.GetAll()
-                .Where(p => p.Id == Guid.Parse("544e9984-7ed5-4c76-10e6-08d70ff62e10"));
+            var playlists = _playlistRepository.GetAll();
+            // .Where(p => p.Id == Guid.Parse("544e9984-7ed5-4c76-10e6-08d70ff62e10"));
             foreach (var playlist in playlists) {
                 await Execute(playlist.Id, context);
             }
