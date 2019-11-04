e0cb5f7a260c030210a7ad31a5688ac636b5905a
Fergal Moran <fergal.moran@gmail.com>
2019-11-04 12:42:10 +0000

    Finally fixed the fucking migrations

---
+++ podnoms-common/Migrations/20191104123801_StatusToNotificationLog.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PodNoms.Comon.Migrations
+{
+    public partial class StatusToNotificationLog : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Succeeded",
+                table: "NotificationLogs",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "ActivityLogPodcastEntry",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreateDate = table.Column<DateTime>(nullable: false, defaultValueSql: "getdate()"),
+                    UpdateDate = table.Column<DateTime>(nullable: false, defaultValueSql: "getdate()"),
+                    PodcastEntryId = table.Column<Guid>(nullable: true),
+                    ClientAddress = table.Column<string>(nullable: true),
+                    ExtraInfo = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActivityLogPodcastEntry", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ActivityLogPodcastEntry_PodcastEntries_PodcastEntryId",
+                        column: x => x.PodcastEntryId,
+                        principalTable: "PodcastEntries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActivityLogPodcastEntry_PodcastEntryId",
+                table: "ActivityLogPodcastEntry",
+                column: "PodcastEntryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ActivityLogPodcastEntry");
+
+            migrationBuilder.DropColumn(
+                name: "Succeeded",
+                table: "NotificationLogs");
+        }
+    }
+}
