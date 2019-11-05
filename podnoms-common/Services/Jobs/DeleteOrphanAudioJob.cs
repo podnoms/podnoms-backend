1dedf380c33f5296c8f712bfcdb54b277b9a8809
Fergal Moran <fergal.moran@gmail.com>
2019-11-05 23:10:57 +0000

    Move blob enumeration to IAsyncEnumerable

--- podnoms-common/Services/Jobs/DeleteOrphanAudioJob.cs
+++ podnoms-common/Services/Jobs/DeleteOrphanAudioJob.cs
@@ -12,7 +12,7 @@ using PodNoms.Common.Persistence.Repositories;
 using PodNoms.Common.Services.Storage;
 
 namespace PodNoms.Common.Services.Jobs {
-    public class DeleteOrphanAudioJob : AbstractHostedJob {
+    public class DeleteOrphanAudioJob : BlobEnumerateJob {
         private readonly IEntryRepository _entryRepository;
         private readonly StorageSettings _storageSettings;
         private readonly AudioFileStorageSettings _audioStorageSettings;
@@ -28,52 +28,43 @@ namespace PodNoms.Common.Services.Jobs {
         public override async Task<bool> Execute(PerformContext context) {
             _setContext(context);
             try {
-                var storageAccount = CloudStorageAccount.Parse(_storageSettings.ConnectionString);
-                var blobClient = storageAccount.CreateCloudBlobClient();
-                var container = blobClient.GetContainerReference(_audioStorageSettings.ContainerName);
+                var container = CloudStorageAccount
+                    .Parse(_storageSettings.ConnectionString)
+                    .CreateCloudBlobClient()
+                    .GetContainerReference(_audioStorageSettings.ContainerName);
+
+                var blobs = GetBlobs<CloudBlockBlob>(container);
+
                 short deletedCount = 0;
                 short blobCount = 0;
-                BlobContinuationToken continuationToken = null;
-                do {
-                    var blobs = await container.ListBlobsSegmentedAsync(
-                        prefix: null,
-                        useFlatBlobListing: true,
-                        blobListingDetails: BlobListingDetails.None,
-                        maxResults: 1000,
-                        currentToken: continuationToken,
-                        options: null,
-                        operationContext: null);
-                    foreach (CloudBlockBlob blob in blobs.Results) {
-                        try {
-                            if (blob.Name.Contains("backup/")) {
-                                continue;
-                            }
 
-                            var url = $"{_audioStorageSettings.ContainerName}/{blob.Name}";
-                            var id = blob.Name.Split('.').First();
+                await foreach (var blob in blobs) {
+                    try {
 
-                            var entry = await _entryRepository.GetAsync(Guid.Parse(id));
-                            if (entry is null) {
-                                await container.RenameAsync(blob.Name, $"backup/{blob.Name}");
-                                //await blob.DeleteIfExistsAsync();
-                                deletedCount++;
-                            }
-                            blobCount++;
-                            Log($"{id} : {blobCount} processed, {deletedCount} deleted.");
-                        } catch (Exception e) {
-                            LogWarning($"Error processing blob {blob.Uri}\n{e.Message}");
+                        if (blob.Name.Contains("backup/")) {
+                            continue;
                         }
+
+                        var url = $"{_audioStorageSettings.ContainerName}/{blob.Name}";
+                        var id = blob.Name.Split('.').First();
+
+                        var entry = await _entryRepository.GetAsync(Guid.Parse(id));
+                        if (entry is null) {
+                            await container.RenameAsync(blob.Name, $"backup/{blob.Name}");
+                            //await blob.DeleteIfExistsAsync();
+                            deletedCount++;
+                        }
+                        blobCount++;
+                        Log($"{id} : {blobCount} processed, {deletedCount} deleted.");
+                    } catch (Exception e) {
+                        LogWarning($"Error processing blob {blob.Uri}\n{e.Message}");
                     }
-
-                    continuationToken = blobs.ContinuationToken;
-                } while (continuationToken != null);
-
+                }
                 Log($"Successfully processed orphans, {blobCount} visited, {deletedCount} deleted.");
                 return true;
             } catch (Exception ex) {
                 LogError($"Error clearing orphans\n{ex.Message}");
             }
-
             return false;
         }
     }
