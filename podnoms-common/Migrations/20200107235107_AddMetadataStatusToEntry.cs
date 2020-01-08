02fc3ceee48f009185e6e231be719bada184af19
Fergal Moran <fergal.moran@gmail.com>
2020-01-08 01:34:38 +0000

    Tagging DONE

---
+++ podnoms-common/Migrations/20200107235107_AddMetadataStatusToEntry.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PodNoms.Comon.Migrations
+{
+    public partial class AddMetadataStatusToEntry : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MetadataStatus",
+                table: "PodcastEntries",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MetadataStatus",
+                table: "PodcastEntries");
+        }
+    }
+}
