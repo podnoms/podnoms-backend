f0a62b1c786146a0203d62b55e02fc2a183d8bc1
Fergal Moran <fergal.moran@gmail.com>
2020-05-06 12:46:43 +0100

    Chat updates

--- podnoms-common/Services/Hubs/ChatHub.cs
+++ podnoms-common/Services/Hubs/ChatHub.cs
@@ -2,7 +2,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
 namespace PodNoms.Common.Services.Hubs {
-    [Authorize(AuthenticationSchemes = "Bearer")]
+    //this needs to allow anonymous access
+    // [Authorize(AuthenticationSchemes = "Bearer")]
     public class ChatHub : Hub {
 
     }
