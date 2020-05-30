e882de7878eecd9f6bb415078b20ce14385ee8a8
Fergal Moran <fergal.moran@gmail.com>
2020-05-30 05:09:33 +0100

    Migrations added

---
+++ podnoms-common/Migrations/20200530034905_AddRefreshTokenInfrastructure.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PodNoms.Comon.Migrations {
+    public partial class AddRefreshTokenInfrastructure : Migration {
+        protected override void Up(MigrationBuilder migrationBuilder) {
+
+            migrationBuilder.CreateTable(
+                name: "RefreshTokens",
+                columns: table => new {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreateDate = table.Column<DateTime>(nullable: false, defaultValueSql: "getdate()"),
+                    UpdateDate = table.Column<DateTime>(nullable: false, defaultValueSql: "getdate()"),
+                    Token = table.Column<string>(nullable: true),
+                    Expires = table.Column<DateTime>(nullable: false),
+                    UserId = table.Column<string>(nullable: true),
+                    RemoteIpAddress = table.Column<string>(nullable: true),
+                    ApplicationUserId = table.Column<string>(nullable: true)
+                },
+                constraints: table => {
+                    table.PrimaryKey("PK_RefreshTokens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RefreshTokens_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RefreshTokens_ApplicationUserId",
+                table: "RefreshTokens",
+                column: "ApplicationUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder) {
+            migrationBuilder.DropTable(
+                name: "RefreshTokens");
+        }
+    }
+}
