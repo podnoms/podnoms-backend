05822fe44925c02a2efb0e5bd4700363763c628a
Fergal Moran <fergal.moran@gmail.com>
2020-06-16 08:28:49 +0100

    Public processing endpoints added

--- podnoms-common/Services/PageParser/RssFeedParser.cs
+++ podnoms-common/Services/PageParser/RssFeedParser.cs
@@ -2,7 +2,10 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using CodeHollow.FeedReader;
+using CodeHollow.FeedReader.Feeds;
+using CodeHollow.FeedReader.Feeds.Itunes;
 using PodNoms.Common.Data.ViewModels.RssViewModels;
+using PodNoms.Common.Utils.Extensions;
 
 namespace PodNoms.Common.Services.Rss {
     public class RssFeedParser {
@@ -10,24 +13,31 @@ namespace PodNoms.Common.Services.Rss {
             var rss = await FeedReader.ReadAsync(url);
             if (rss.Type == FeedType.Rss_2_0) {
                 var feed = (CodeHollow.FeedReader.Feeds.Rss20Feed)rss.SpecificFeed;
+                var podcastFeed = rss.GetItunesChannel();
+
                 var ret = new PodcastEnclosureViewModel {
                     Title = feed.Title,
                     Description = feed.Description,
+                    Author = podcastFeed.Author,
+                    Category = feed.Categories.Join("\n"),
                     Link = feed.Link,
-                    // Image = feed.ImageUrl,
-                    // PublishDate = feed.LastUpdatedDateString,
+                    Image = feed.Image.Link ?? podcastFeed.Image.Href,
+                    PublishDate = feed.PublishingDateString,
                     Language = feed.Language,
                     Copyright = feed.Copyright,
-                    OwnerEmail = feed.Title,
-                    Items = feed.Items.Select(item => new PodcastEnclosureItemViewModel {
-                        // Uid = item.Id,
-                        Title = item.Title,
-                        // Description = item.Description,
-                        // Author = item.Author,
-                        // UpdateDate = item.PublishingDateString,
-                        // AudioUrl = item.SpecificItem["enclosure"],
-                        // Duration = item.Id,
-                    }).ToList()
+                    Owner = podcastFeed.Owner.Name,
+                    OwnerEmail = podcastFeed.Owner.Email,
+                    Items = feed.Items
+                        .Cast<Rss20FeedItem>()
+                        .Select(item => new PodcastEnclosureItemViewModel {
+                            Uid = item.Guid,
+                            Title = item.Title,
+                            Description = item.Description,
+                            Author = item.Author ?? "",
+                            UpdateDate = item.PublishingDateString,
+                            AudioUrl = item.Enclosure.Url,
+                            // Duration = item.Id,
+                        }).ToList()
                 };
                 return ret;
             }
