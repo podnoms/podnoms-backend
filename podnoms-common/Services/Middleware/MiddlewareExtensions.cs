b6c30ab62f97e5bfc30b54e1de99dad5ed3b8e0a
Fergal Moran <fergal.moran@gmail.com>
2020-06-18 00:46:09 +0100

    Remove unneeded redirect middleware

--- podnoms-common/Services/Middleware/MiddlewareExtensions.cs
+++ podnoms-common/Services/Middleware/MiddlewareExtensions.cs
@@ -4,7 +4,6 @@ namespace PodNoms.Common.Services.Middleware {
     public static class MiddlewareExtensions {
         public static IApplicationBuilder UseCustomDomainRedirect(this IApplicationBuilder app) {
             return app
-                .UseMiddleware<CustomDomainRedirectMiddleware>()
                 .UseMiddleware<SharingLinkRedirectMiddleware>();
         }
     }
