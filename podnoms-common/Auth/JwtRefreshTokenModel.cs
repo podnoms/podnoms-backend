6667b73510be7de79ea2e6284870a4bddb7fb9f1
Fergal Moran <fergal.moran@gmail.com>
2020-06-22 15:33:07 +0100

    Stop refresh token error

---
+++ podnoms-common/Auth/JwtRefreshTokenModel.cs
@@ -0,0 +1,14 @@
+﻿namespace PodNoms.Common.Auth
+{
+    public class JwtRefreshTokenModel {
+        public JwtRefreshTokenModel() {
+        }
+
+        public JwtRefreshTokenModel(string refresh, JwtTokenModel jwt) {
+            this.Refresh = refresh;
+            this.Jwt = jwt;
+        }
+        public string Refresh { get; set; }
+        public JwtTokenModel Jwt { get; set; }
+    }
+}
