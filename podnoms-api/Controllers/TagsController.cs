c19d7c508f7a205670e6ccd86ab6292505777bfc
Fergal Moran <fergal.moran@gmail.com>
2020-10-29 14:41:52 +0000

    M2M models & migrations added

--- podnoms-api/Controllers/TagsController.cs
+++ podnoms-api/Controllers/TagsController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using PodNoms.Common.Data.ViewModels.Resources;
+using PodNoms.Common.Persistence;
 using PodNoms.Common.Persistence.Repositories;
 using PodNoms.Data.Models;
 
@@ -18,21 +19,32 @@ namespace PodNoms.Api.Controllers {
     [ApiController]
     public class TagsController : BaseAuthController {
         private readonly ITagRepository _repository;
+        private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
 
         public TagsController(IHttpContextAccessor contextAccessor, UserManager<ApplicationUser> userManager,
-            ILogger<CategoryController> logger, ITagRepository repository, IMapper mapper)
+            ILogger<CategoryController> logger, ITagRepository repository, IUnitOfWork unitOfWork, IMapper mapper)
             : base(contextAccessor, userManager, logger) {
             _repository = repository;
+            _unitOfWork = unitOfWork;
             _mapper = mapper;
         }
-        
+
         [HttpGet]
         public async Task<ActionResult<List<TagViewModel>>> GetTags() {
             var tags = await _repository.GetAll()
                 .Take(10)
                 .ToListAsync();
             return _mapper.Map<List<EntryTag>, List<TagViewModel>>(tags);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TagViewModel>> AddTag([FromQuery] string tagName) {
+            var tag = new EntryTag(tagName);
+            tag = _repository.AddOrUpdate(tag);
+            await _unitOfWork.CompleteAsync();
+
+            return _mapper.Map<EntryTag, TagViewModel>(tag);
         }
     }
 }
--- podnoms-data/Models/EntryTag.cs
+++ podnoms-data/Models/EntryTag.cs
@@ -2,7 +2,12 @@
 
 namespace PodNoms.Data.Models {
     public class EntryTag : BaseEntity {
+        public EntryTag(string tagName) {
+            this.TagName = tagName;
+        }
+
         public string TagName { get; set; }
+
         //back relationship
         public ICollection<PodcastEntry> Entries { get; set; } = new List<PodcastEntry>();
     }
